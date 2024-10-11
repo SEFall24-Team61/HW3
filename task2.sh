c4448c498fb7f1a6d31d252a01d02bc73111dfc8
Shruti Dhond <42238433+shrutid02@users.noreply.github.com>
2024-10-10 21:38:00 -0400

    Added task2.sh

---
+++ task2.sh
@@ -0,0 +1,1 @@
+cd dataset1 && grep -l "sample" * | xargs grep -l "CSC510" | xargs grep -o "CSC510" | uniq -c | grep -E '^[[:space:]]*[3-9][0-9]* ' | cut -d: -f1 | gawk '{ cmd = "stat -f %z \"" $2 "\""; cmd | getline size; close(cmd); print size, $1, $2}' | sort -k2,2nr -k1,1nr | gawk '{print $3}' | sed 's/file/filtered/g'
