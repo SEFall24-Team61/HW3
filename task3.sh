ff8b8ed2b80745276e137d2b5020f507744f9500
Shardul Khare <ludraa@Sharduls-Air.lan>
2024-10-10 23:15:28 -0400

    Task3 Changed to Pipeline

--- task3.sh
+++ task3.sh
@@ -1,16 +1,5 @@
-awk -F',' '
-NR > 1 && $3 == 2 && substr($13, 1, 1) == "S" {
-    gsub(",male", ",M");
-    gsub(",female", ",F");
-    if ($7 != "") {
-        total += $7;
-        count++;
-    }
-} 
-END { 
-    if (count > 0) 
-        print "Average Age is", total / count
-    else 
-        print "Average Age is N/A"
-}' titanic.csv
-
+awk -F',' 'NR > 1 && $3 == 2 && substr($13, 1, 1) == "S" { print }' titanic.csv |  # Skip header, filter 2nd class & Southampton
+sed 's/,male/,M/; s/,female/,F/' |             # Replace gender labels
+awk -F',' '{ if ($7 != "") { total += $7; count++ } }
+           END { if (count > 0) print "Average Age is", total / count; 
+                 else print "Average Age is N/A" }'  # Calculate average age
