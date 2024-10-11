b1fa3203fb2a6d7352bbf5e38628f0b38da2986c
Shardul Khare <ludraa@Sharduls-Air.lan>
2024-10-10 23:21:45 -0400

    Task3 Changed to Pipeline, changed female first

--- task3.sh
+++ task3.sh
@@ -1,5 +1,5 @@
 awk -F',' 'NR > 1 && $3 == 2 && substr($13, 1, 1) == "S" { print }' titanic.csv |  # Skip header, filter 2nd class & Southampton
-sed 's/,male/,M/; s/,female/,F/' |             # Replace gender labels
+sed 's/,female/,F/; s/,male/,M/' |             
 awk -F',' '{ if ($7 != "") { total += $7; count++ } }
            END { if (count > 0) print "Average Age is", total / count; 
                  else print "Average Age is N/A" }'  # Calculate average age
