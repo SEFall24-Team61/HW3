d30a2ebe7f4d5f3f0ebbe085607de52471ecbdc1
Shruti Dhond <42238433+shrutid02@users.noreply.github.com>
2024-10-28 23:40:42 -0400

    Added print statement for task3.sh

--- task3.sh
+++ task3.sh
@@ -1,5 +1,5 @@
 awk -F',' 'NR > 1 && $3 == 2 && substr($13, 1, 1) == "S" { print }' titanic.csv |  # Skip header, filter 2nd class & Southampton
 sed 's/,female/,F/; s/,male/,M/' |             
-awk -F',' '{ if ($7 != "") { total += $7; count++ } }
+awk -F',' '{ if ($7 != "") { total += $7; count++ } print $0 }
            END { if (count > 0) print "Average Age is", total / count; 
                  else print "Average Age is N/A" }'  # Calculate average age
